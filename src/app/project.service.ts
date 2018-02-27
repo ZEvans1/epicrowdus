48608102d4a05aa9b99c461e96f4fc412605f3a3
Adam Calhoun and Zach Evans <zach@>
2018-02-27 10:43:46 -0800

    create Project service

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Project } from './project.model'
 
 @Injectable()
 export class ProjectService {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.projects = database.list('projects');
+  }
+
+  getProjects(){
+    return this.projects;
+  }
+
+  addProject(newProject: Project){
+    this.projects.push(newProject);
+  }
+
+  getProjectById(projectId: string){
+    return this.database.object('projects/' + projectId);
+  }
+
+  updateProject(localUpdatedProject){
+    var projectEntryInFirebase = this.getProjectById(localUpdatedProject.$key);
+    projectEntryInFirebase.update({title: localUpdatedProject.title,
+                                description: localUpdatedProject.description});
+  }
+
+  deleteProject(localProjectToDelete){
+    var projectEntryInFirebase = this.getProjectById(localProjectToDelete.$key);
+    projectEntryInFirebase.remove();
+  }
 
 }
