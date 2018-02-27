a943973ab43934b122caf45013d2c540f0857e28
Adam Calhoun and Zach Evans <zach@>
2018-02-27 10:32:05 -0800

    create welcome component, connect to Firebase

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { Router } from '@angular/router';
+import { ProjectService } from '../project.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
+  providers: [ProjectService]
 })
 export class WelcomeComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProject();
+  }
+
+  goToDetailPage(clickedProject){
+    this.router.navigate(['projects', clickedProject.$key])
   }
 
 }
